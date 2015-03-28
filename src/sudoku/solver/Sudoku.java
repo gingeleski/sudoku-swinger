14552f1517957c570266d75e4c0e91f06fd7f3e4
Randy Gingeleski <rjg26247@gmail.com>
2015-03-28 17:25:43 +0000

    wrote bruteforce solver logic, takes forever, no UI yet

--- src/sudoku/solver/Sudoku.java
+++ src/sudoku/solver/Sudoku.java
@@ -7,24 +7,85 @@ public class Sudoku {
     
     // User enters this grid
     static int[][] userGrid = new int[][] {
-        {1, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        {0, 0, 0, 7, 0, 0, 0, 0, 8},
+        {0, 4, 0, 0, 0, 0, 0, 6, 1},
+        {0, 0, 0, 1, 2, 0, 0, 3, 0},
+        {7, 9, 0, 0, 6, 0, 0, 0, 0},
+        {8, 0, 0, 0, 0, 0, 0, 0, 3},
+        {0, 3, 0, 0, 8, 0, 0, 7, 2},
+        {0, 6, 0, 0, 9, 7, 0, 0, 0},
+        {3, 2, 0, 0, 0, 0, 0, 5, 0},
+        {5, 0, 0, 0, 0, 4, 0, 0, 0}};
         
     // Computer experiments on this grid to solve
     static int[][] computerGrid = new int[9][9];
-        
-    static String[][] possibilitiesGrid = new String[9][9];
     
     public static void main(String[] args) {
         
-        printToConsole(userGrid);
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
+                computerGrid[i][j] = userGrid[i][j];
+            }
+        }
+        
+        printToConsole(computerGrid);
+        double timeStart = System.currentTimeMillis();
+        System.out.print("\n\n");
+        computerGrid = solverLoop(0,0,computerGrid);
+        printToConsole(computerGrid);
+        double timeEnd = System.currentTimeMillis();
+        System.out.println("That took " + (timeEnd - timeStart));
+        System.out.print(" milliseconds to complete.");
+    }
+    
+    /**
+     * Recursively bruteforces the Sudoku grid for a valid solution.
+     * 
+     * @param y
+     * @param x
+     * @param grid
+     * @return Solved grid
+     */
+    public static int[][] solverLoop(int y, int x, int[][] grid) {
+        
+        // While not solved
+        while ((!testValid(8, 8, grid)) || (grid[8][8] == 0)) {
+        
+            if(userGrid[y][x] != 0) {
+                
+                int yy, xx;
+                
+                if (x == 8) { yy = y + 1; xx = 0; }
+                else { yy = y; xx = x + 1; }
+                
+                solverLoop(yy, xx, grid); // Recursion
+                
+            } else {
+                
+                if (grid[y][x] < 9) { // Going forward
+                    
+                    grid[y][x]++;
+                    
+                    if(testValid(y, x, grid)) {
+                        
+                        int yy, xx;
+                        
+                        if (x == 8) { yy = y + 1; xx = 0; }
+                        else { yy = y; xx = x + 1; }
+                        
+                        solverLoop(yy, xx, grid); // Recursion
+                    
+                    } else {
+                        
+                        grid[y][x] = 0;
+                        break;
+                    }
+                }
+                
+            }
+        }
+        
+        return computerGrid;
     }
     
     /**
@@ -64,9 +125,8 @@ public class Sudoku {
     public static void printToConsole(int[][] grid) {
         
         for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                System.out.print(grid[i][j] + " ");
-            }
+            
+            for (int j = 0; j < 9; j++) { System.out.print(grid[i][j] + " "); }
             
             System.out.print("\n");
         }
